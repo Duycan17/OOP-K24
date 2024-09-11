6aa1183fb11b8a65be3d6d91aaf895a4b2bf2002
duycan17 <duydk.22git@vku.udn.vn>
2024-09-11 18:58:55 +0700

    Method sample

--- src/Week1/Main.java
+++ src/Week1/Main.java
@@ -2,9 +2,28 @@ package Week1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        double a = 1, b = -3, c = 2; // Ví dụ: phương trình x^2 - 3x + 2 = 0
+        double[] roots = solveQuadraticEquation(a, b, c);
+        if (roots.length == 2) {
+            System.out.println("Phương trình có hai nghiệm phân biệt: x1 = " + roots[0] + ", x2 = " + roots[1]);
+        } else if (roots.length == 1) {
+            System.out.println("Phương trình có nghiệm kép: x = " + roots[0]);
+        } else {
+            System.out.println("Phương trình vô nghiệm.");
+        }
+    }
+
+    public static double[] solveQuadraticEquation(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{x1, x2};
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            return new double[]{x};
+        } else {
+            return new double[]{};
         }
     }
 }
