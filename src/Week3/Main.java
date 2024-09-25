0083f56d1637081cb722af3f40b07d9794b7c922
duycan17 <duydk.22git@vku.udn.vn>
2024-09-25 16:06:56 +0700

    Main class updated

--- src/Week3/Main.java
+++ src/Week3/Main.java
@@ -2,6 +2,14 @@ package Week3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Student student = new Student();
+        student.setName("John");
+        student.setAge(20);
+        student.setAddress("123 Main St");
+        student.setMathScore(90);
+        student.setEnglishScore(80);
+        System.out.println(student.toString());
+        System.out.println("Average score: " + student.getAverageScore());
+        System.out.println(student.getAverageScore());
     }
 }
