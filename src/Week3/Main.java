17b2ece64bebce6d55eb96b13d60134e7e071116
duycan17 <duydk.22git@vku.udn.vn>
2024-10-09 15:42:11 +0700

    Changed main

--- src/Week3/Main.java
+++ src/Week3/Main.java
@@ -1,10 +1,17 @@
 package Week3;
 
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("John", 20, "123 Main St", 90, 80);
-        System.out.println(student.toString());
-        System.out.println("Average score: " + student.getAverageScore());
-        System.out.println(student.getAverageScore());
+        Vector<Student> students = new Vector<Student>();
+        students.add(new Student("Nguyen Van A", 20, "Ha Noi", 9, 8));
+        students.add(new Student("Nguyen Van B", 21, "Ha Noi", 7, 8));
+        Student student1 = new Student("Nguyen Van C",20,"Ha Noi",8,9);
+        students.add(student1);
+        Student student2 = new Student("Nguyen Van D",21,"Ha Noi",9,9);
+        students.add(student2);
+        students.add(new Student("Nguyen Van E", 22, "Ha Noi", 10, 10));
+
     }
 }
